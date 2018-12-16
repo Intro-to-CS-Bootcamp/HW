77062be6275c1277a2a839272efbf543021fc3bc
Catherine Hu <catherine.gobears@berkeley.edu>
2018-12-15 17:27:06 -0800

    ch08 skeleton

---
+++ ch08/array_sort.rb
@@ -0,0 +1,21 @@
+# Prompts for user input of words, store them in an array
+# Ends adding to the array when ENTER is pressed on an empty line
+# Prints the sorted version of the array
+# Upper/Lower cases should not affect the word sequence 
+
+## FILL IN WITH YOUR CODE
+puts("Type in words and I can sort them.")
+puts("Press ENTER on an empty line to finish.")
+
+words = []
+while (true)
+    word = 
+    if ()
+        break
+    end
+
+    ## ADD MORE?
+end
+
+puts("Here are the sorted words:")
+puts() ## FIX ME
---
+++ ch08/table_of_contents.rb
@@ -0,0 +1,14 @@
+# Print a formatted table of contents using arrays
+
+## FILL IN WITH YOUR CODE
+
+title = "Table of Contents"
+chapters = ## ARRAY
+puts(title.center(50))
+puts 
+
+chapter_num = 1
+chapter.each do |chapter|
+    ## FILL IN
+
+end
