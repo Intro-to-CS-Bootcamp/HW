58d4c850745dd522bc6ba0c4d9315a8347693814
Alexander Wu <alexanderwu68@yahoo.com>
2019-01-14 12:23:00 +0800

    Fix indentation

--- ch10/array_max.rb
+++ ch10/array_max.rb
@@ -2,17 +2,17 @@
 # Don't forget to use array indexing and array.pop() to remove the last element of an array
 
 def array_max(array)
-    if array.length == 1  # base case
-        return _________
+  if array.length == 1  # base case
+    return _________
+  else
+    last = _________
+    rest = _________________  # recursive call - array is now shorter
+    if last > rest
+      return ______
     else
-        last = _________
-        rest = _________________  # recursive call
-        if last > rest
-            return ______
-        else
-            return ______
-        end
+      return ______
     end
+  end
 end
 
 puts(array_max([3, 5, -2, 8, 4])) # expected: 8
