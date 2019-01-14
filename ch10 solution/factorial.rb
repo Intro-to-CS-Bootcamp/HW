9fce54224c8e36b48da8482272aaa37973cfecb6
Alexander Wu <alexanderwu68@yahoo.com>
2019-01-14 17:35:13 +0800

    Add prompt for factorial

--- ch10 solution/factorial.rb
+++ ch10 solution/factorial.rb
@@ -1,3 +1,6 @@
+# For this exercise we will not provide any skeleton code (other than the method declaration).
+# See if you can figure out how to write the method!
+
 def factorial(n)
   if n == 0  # base case
     return 1
